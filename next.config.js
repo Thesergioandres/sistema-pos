e7832601a93ca25fac70d9e7fff95e522835d38e
YeferRojas <yefersonrojas16yi@gmail.com>
2025-09-08 16:04:40 -0500

    chore(env): añadir variables Cognito (VITE_ y NEXT_PUBLIC_*) y exponerlas en Next config

--- next.config.js
+++ next.config.js
@@ -13,6 +13,12 @@ const withPWA = nextPwa({
 const nextConfig = {
   // Ayuda a Next a resolver la raíz correcta del proyecto cuando hay múltiples lockfiles
   outputFileTracingRoot: path.join(process.cwd()),
+  env: {
+    // Aseguramos disponibilidad en el cliente en tiempo de build
+    NEXT_PUBLIC_COGNITO_REGION: process.env.NEXT_PUBLIC_COGNITO_REGION,
+    NEXT_PUBLIC_COGNITO_USER_POOL_ID: process.env.NEXT_PUBLIC_COGNITO_USER_POOL_ID,
+    NEXT_PUBLIC_COGNITO_CLIENT_ID: process.env.NEXT_PUBLIC_COGNITO_CLIENT_ID,
+  },
 };
 
 export default withPWA(nextConfig);
